8d56a9edbb7869629dce32905ac050944a554c7f
Nicolas T <tiranico.pro@gmail.com>
2017-07-24 15:41:54 +0200

    Update app.js

--- app.js
+++ app.js
@@ -7,4 +7,4 @@ total = CA - RSI - elec;
 return alert("CA = " + CA + "\nElec = " + elec + "\nChiffre post-electricite = " + chiffreMoinsElec + "\nRSI (25% de chiffre post-elec) = " + RSI + "\nTotal = " + total);
 };
 
-calcul(document.getElementById("CA").value, document.getElementById("elec").value);
+calcul(parseInt(document.getElementById("CA").value), parseInt(document.getElementById("elec").value));
